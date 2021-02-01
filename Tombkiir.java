acfc7ded22ab3e4bfeff971e77d480b2cdbb248f
baromatyo <baromatyo@gmail.com>
2021-02-01 11:29:32 +0100

    Add files via upload

--- Tombkiir.java
+++ Tombkiir.java
@@ -11,6 +11,25 @@ import java.util.Formatter;
  * @author User
  */
 public class Tombkiir {
+    
+    static void kiir(int tomb[]){
+        for(int i=0;i<tomb.length;i++){
+            System.out.print(String.format("%,4d",tomb[i])+" ");
+        }
+    }
+    static void bubbleSort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (arr[j] > arr[j+1]) 
+                { 
+                    // swap arr[j+1] and arr[j] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
+    } 
 
     /**
      * @param args the command line arguments
@@ -25,18 +44,20 @@ public class Tombkiir {
             tomb2[i] = (int) (Math.random()*200)-100;
             tomb3[i] = (int) (Math.random()*200)-100;
         }
-        for(int i=0;i<10;i++){
-            System.out.print(String.format("%,4d",tomb[i])+" ");
-        }
-        System.out.println("");
-        for(int i=0;i<10;i++){
-            System.out.print(String.format("%,4d",tomb2[i])+" ");
-        }
-        System.out.println("");
-        for(int i=0;i<10;i++){
-            System.out.print(String.format("%,4d",tomb3[i])+" ");
-        }
-        
+        kiir(tomb);
+        System.out.println("\n");
+        kiir(tomb2);
+        System.out.println("\n");
+        kiir(tomb3);
+        System.out.println("\nsorbarendezve: \n");
+        bubbleSort(tomb);
+        kiir(tomb);
+        System.out.println("\n");
+        bubbleSort(tomb2);
+        kiir(tomb2);
+        System.out.println("\n");
+        bubbleSort(tomb3);
+        kiir(tomb3);
     }
     
 }
